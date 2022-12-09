cfa3002a5fa0f05dc4467bd033d77070fe773578
Nguyen Tien Thinh <ltphuongunited@gmail.com>
2022-12-09 22:48:47 +0700

    modify labels_dict

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ def render_contact():
 @app.route('/predict', methods=['POST'] )
 def predict(path_image):
     print(path_image)
-    labels_dict = {'0': 'A', '1': 'B', '2': 'C', '3': 'D', '4': 'E', '5': 'F', '6': 'G', '7': 'H', '8': 'I', '9': 'J', '10': 'K', '11': 'L', '12': 'M', '13': 'N', '14': 'O', '15': 'P', '16': 'Q', '17': 'R', '18': 'S', '19': 'T', '20': 'U', '21': 'V', '22': 'W', '23': 'X', '24': 'Y', '25': 'Z', '26': 'space', '27': 'del', '28': 'nothing'}
+    labels_dict = {'0': 'A', '1': 'B', '2': 'C', '3': 'D', '4': 'E', '5': 'F', '6': 'G', '7': 'H', '8': 'I', '9': 'J', '10': 'K', '11': 'L', '12': 'M', '13': 'N', '14': 'O', '15': 'P', '16': 'Q', '17': 'R', '18': 'S', '19': 'T', '20': 'U', '21': 'V', '22': 'W', '23': 'X', '24': 'Y', '25': 'Z', '26': 'del', '27': 'nothing', '28': 'space'}
 
     img = load_img(path_image)
     model = keras.models.load_model('model.h5')
